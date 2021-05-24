9c342fb32fd50aa3c20cacb14dad557773c92ab8
JitkaKamalae <Jitkakamalae@gmail.com>
2021-05-24 08:17:59 +0200

    uloha 5

--- hra.js
+++ hra.js
@@ -18,7 +18,88 @@ const Play = (event) => {
       square.disabled = true;
     }
   }
+  if (isWinningMove(event.target)) {
+    setTimeout(() => {
+      if (player === 'cross') {
+        const conf = confirm(`Vítězí kolečko.`);
+        if (conf) {
+          location.reload();
+        }
+      } else {
+        const conf = confirm(`Vížězí křížek.`);
+        if (conf) {
+          location.reload();
+        }
+      }
+    }, 250);
+  }
 };
 
 let box = document.querySelectorAll('.butKlik');
 box = addEventListener('click', Play);
+const getSymbol = (field) => {
+  if (field.classList.contains('sachovnice--circle')) {
+    return 'circle';
+  } else if (field.classList.contains('sachovnice--cross')) {
+    return 'cross';
+  }
+};
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.butKlik');
+
+const getField = (row, column) => {
+  return fields[row * boardSize + column];
+};
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+    fieldIndex++;
+  }
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+  let i;
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+  let inColumn = 1;
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+  return false;
+};
